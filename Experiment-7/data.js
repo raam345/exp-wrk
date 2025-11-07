7578932899296884d69e5c7d41a088819b500fe2
Ganugapati Srirama Kowndinya Hemanth Kumar <2400032580@kluniversity.in>
2025-11-08 01:30:06 +0530

    workbook

---
+++ Experiment-7/data.js
@@ -0,0 +1,9 @@
+// Basic JSON-like data exported as a module
+export const books = [
+    { id: 1, title: "Eloquent JavaScript", author: "Marijn Haverbeke", price: 499, availability: "in stock" },
+    { id: 2, title: "You Don't Know JS", author: "Kyle Simpson", price: 399, availability: "in stock" },
+    { id: 3, title: "Clean Code", author: "Robert C. Martin", price: 599, availability: "out of stock" },
+    { id: 4, title: "JavaScript: The Good Parts", author: "Douglas Crockford", price: 299, availability: "in stock" }
+];
+
+
