d63c54c482e7122ef25dac8987365b169dec52cd
Ganugapati Srirama Kowndinya Hemanth Kumar <2400032580@kluniversity.in>
2025-11-08 20:40:04 +0530

    workbook

--- Experiment-10/vite-project/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import feedbackReducer from '../features/feedbackslice';
-
-export const store = configureStore({
-  reducer: {
-    feedback: feedbackReducer,
-  },
-});
